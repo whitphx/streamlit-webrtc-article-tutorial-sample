ee8bae1dadf7610b4298b879d391d9c4686b1990
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-09-02 21:13:56 +0900

    Update to use the class-less callback API which has been introduced since 0.40.0

--- app.py
+++ app.py
@@ -6,27 +6,22 @@ import cv2
 st.title("My first Streamlit app")
 st.write("Hello, world")
 
-
-class VideoProcessor:
-    def __init__(self) -> None:
-        self.threshold1 = 100
-        self.threshold2 = 200
-
-    def recv(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-
-        img = cv2.cvtColor(cv2.Canny(img, self.threshold1, self.threshold2), cv2.COLOR_GRAY2BGR)
-
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
+threshold1 = st.slider("Threshold1", min_value=0, max_value=1000, step=1, value=100)
+threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=200)
 
 
-ctx = webrtc_streamer(
+def callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    img = cv2.cvtColor(cv2.Canny(img, threshold1, threshold2), cv2.COLOR_GRAY2BGR)
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+webrtc_streamer(
     key="example",
-    video_processor_factory=VideoProcessor,
-    rtc_configuration={
+    video_frame_callback=callback,
+    rtc_configuration={  # Add this line
         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
     }
 )
-if ctx.video_processor:
-    ctx.video_processor.threshold1 = st.slider("Threshold1", min_value=0, max_value=1000, step=1, value=100)
-    ctx.video_processor.threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=200)

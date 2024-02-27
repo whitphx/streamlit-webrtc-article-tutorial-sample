7dc89929d67d51bb6c012250ecc86d40bf7c0e5c
hori0926 <hirotaka.h09@gmail.com>
2024-02-28 03:49:15 +0900

    wip

--- callbacks.py
+++ callbacks.py
@@ -4,13 +4,20 @@ from pathlib import Path
 
 import av
 import pydub
-import numpy as np
+import os
+
 
 import streamlit as st
 
 
-HERE = Path(__file__).parent
-ROOT = HERE.parent
+RESPONSE_DIR = Path("./responses")
+
+FRAME_SETTINGS = {
+            "samples": 960,
+            "sample_format": "s16",
+            "channels": 2,
+            "sample_rate": 48000,
+        }
 
 logger = logging.getLogger(__name__)
 
@@ -39,13 +46,14 @@ def silent_audio_frame(frame: av.AudioFrame) -> av.AudioFrame:
     return silent_frame
 
 def audio_frame_callback(frame: av.AudioFrame) -> av.AudioFrame:
-    if st.session_state.recording:
+    if st.session_state.recording or "talk_id" not in st.session_state:
         return silent_audio_frame(frame)
     
     else :
         """
             ここで行われること
             ・現在流している音声ファイルとその位置を把握
+            
             ・音声が続く場合、その音声を流す
             ・音声が終わった場合、次の音声を流す
             ・まだ収録していない場合、全て再生し終わった場合、無音を返す
@@ -64,4 +72,24 @@ def audio_frame_callback(frame: av.AudioFrame) -> av.AudioFrame:
             ・frame_number: 再生している音声ファイルのフレーム位置
             ・is_finished: 返信音声が全てファイルかされているかどうか
         """
-    
+        if "file_number" not in st.session_state:
+            st.session_state["file_number"] = 0
+        if "frame_number" not in st.session_state:
+            st.session_state["frame_number"] = 0
+        if "is_finished" not in st.session_state:
+            st.session_state["is_finished"] = False
+        
+        
+        stream_frame = st.session_state[st.session_state["talk_id"]][st.session_state["file_number"]][st.session_state["frame_number"]]
+        if st.session_state["frame_number"] <  len(st.session_state[st.session_state["talk_id"]][st.session_state["file_number"]])-1:
+            st.session_state["frame_number"] += 1
+        elif st.session_state["file_number"] < len(st.session_state[st.session_state["talk_id"]])-1:
+            st.session_state["file_number"] += 1
+            st.session_state["frame_number"] = 0
+        else:
+            st.session_state["file_number"] = 0
+            st.session_state["frame_number"] = 0
+            st.session_state["is_finished"] = True
+            return silent_audio_frame(frame)
+        
+        return stream_frame

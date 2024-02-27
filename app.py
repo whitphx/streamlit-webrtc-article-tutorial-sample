db616e4cc9935b9774425cdd670e7428462f6455
hori0926 <hirotaka.h09@gmail.com>
2024-02-28 01:52:37 +0900

    fix

--- app.py
+++ app.py
@@ -91,5 +91,8 @@ while webrtc_ctx.audio_receiver:
             channels=len(audio_frame.layout.channels),
             )
             sound_chunk += sound
-sound_chunk.export("test.wav", format="wav")
+            
+sound_chunk.export(f"./records/{st.session_state.talk_id}.wav", format="wav")
 logger.warning("Audio file is saved.")
+sound_chunk = pydub.AudioSegment.empty()
+st.session_state["talk_id"] = str(uuid.uuid4())

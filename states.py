b5a88b56fc6001f4cba4479fbec85273e308d3f4
hori0926 <hirotaka.h09@gmail.com>
2024-02-28 18:58:35 +0900

    state

---
+++ states.py
@@ -0,0 +1,16 @@
+class StatesObject:
+    def __init__(self, is_recpording=False, talk_id=None, file_number=0, frame_number=0, is_finished=False):
+        self.is_recpording = False
+        self.talk_id = None
+        self.is_finished = False
+        self.file_number = 0
+        self.frame_number = 0
+        
+    def get_is_recpording(self):
+        return self.is_recpording
+    
+    def get_talk_id(self):
+        return self.talk_id
+    
+    def set_talk_id(self, talk_id):
+        self.talk_id = talk_id
